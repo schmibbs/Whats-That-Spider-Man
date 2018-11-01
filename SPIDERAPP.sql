68c7709b265fa50d99bc12a509a60f5f876b0d8e
schmibbs <32438797+schmibbs@users.noreply.github.com>
2018-11-01 01:08:35 -0400

    Added fields and examples
    Need to work on identifying keys

--- SPIDERAPP.sql
+++ SPIDERAPP.sql
@@ -1,17 +1,17 @@
 DROP TABLE IF EXISTS Location;
 DROP TABLE IF EXISTS Spider;
 DROP TABLE IF EXISTS Habitat;
+DROP TABLE IF EXISTS Color;
 
 CREATE TABLE Location (
-latitude DECIMAL(3, 2),
-longitude DECIMAL(3, 2)
+latitude DECIMAL(9, 6),
+longitude DECIMAL(9, 6)
 );
 
 CREATE TABLE Spider (
 commonname char(100) PRIMARY KEY,
 isvenomous bool,
-photo BLOB(10),
-color char(15)
+photo BLOB(10)
 );
 
 CREATE TABLE Habitat(
@@ -22,9 +22,27 @@ region char(30),
 city char(30)
 );
 
+CREATE TABLE Color (
+primarycolor char(25),
+secondarycolor char(25)
+);
+
+INSERT INTO Habitat 
+VALUES(
+"North America", "America", "united states", "south florida", "miami"
+);
+
 INSERT INTO Spider 
 VALUES
-("Daddy long legs", FALSE, NULL, "brown");
+("Spiny orb weaver", FALSE, NULL),
+("Huntsman", FALSE, NULL),
+("Carolina wolf", FALSE, NULL),
+("Southern Black Widow", TRUE, NULL);
 
-select *
-from spider;
+INSERT INTO Location
+VALUES
+(25.758916, -80.373866);
+
+select commonname, isvenomous
+from spider, Location, Habitat
+where Habitat.city = "miami";
